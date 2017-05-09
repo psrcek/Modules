2dd5e15a75c507b5028edc67355e8438c7acb507
Pepich <benedikt.abel@yahoo.de>
2017-05-09 18:27:39 +0200

    Fixed message.cmd

--- src/com/redstoner/modules/message/Message.cmd
+++ src/com/redstoner/modules/message/Message.cmd
@@ -1,17 +1,17 @@
 command message {
+    alias m;
+    alias em;
+    alias msg;
+    alias emsg;
+    alias t;
+    alias et;
+    alias tell;
+    alias etell;
+    alias w;
+    alias ew;
+    alias whisper;
+    alias ewhisper;
     [string:player] [string:message...] {
-        alias m;
-        alias em;
-        alias msg;
-        alias emsg;
-        alias t;
-        alias et;
-        alias tell;
-        alias etell;
-        alias w;
-        alias ew;
-        alias whisper;
-        alias ewhisper;
         run message player message;
         help Sends a direct message to a player.;
         perm utils.message;
@@ -19,10 +19,10 @@ command message {
 }
 
 command reply {
+    alias r;
+    alias er;
+    alias ereply;
     [string:message...] {
-        alias r;
-        alias er;
-        alias ereply;
         run reply message;
         help Sends a direct message to the last person you talked to.;
         perm utils.message;
