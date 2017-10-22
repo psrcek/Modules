ad9bc47b7e051e74ae326b300970ae6be226982f
Pepich <benedikt.abel@yahoo.de>
2017-10-22 14:56:18 +0200

    Fixed ips flag

--- src/com/redstoner/modules/seen/Seen.cmd
+++ src/com/redstoner/modules/seen/Seen.cmd
@@ -1,4 +1,10 @@
 command seen {
+     [string:player] {
+        help Displays information about a player.;
+        perm utils.seen;
+        run seen player;
+    }
+    
     [string:player] [flag:ips] {
         help Displays information about a player.;
         perm utils.seen;
