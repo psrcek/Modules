452b738e2d73da65a02a15e037ab0e0a2d113116
Pepich <benedikt.abel@yahoo.de>
2018-03-12 03:26:08 +0100

    Fixed ConcurrentModificationException

--- src/com/redstoner/modules/datamanager/DataManager.java
+++ src/com/redstoner/modules/datamanager/DataManager.java
@@ -36,7 +36,7 @@ import com.redstoner.modules.Module;
 
 @Commands(CommandHolderType.Stream)
 @AutoRegisterListener
-@Version(major = 4, minor = 1, revision = 7, compatible = 4)
+@Version(major = 4, minor = 1, revision = 8, compatible = 4)
 public final class DataManager implements CoreModule, Listener
 {
 	protected final File dataFolder = new File(Main.plugin.getDataFolder(), "data");
@@ -86,13 +86,13 @@ public final class DataManager implements CoreModule, Listener
 			{
 				for (String id : scheduled_saves)
 				{
-					scheduled_saves.remove(id);
 					Object raw = data.get(id);
 					if (raw == null || ((JSONObject) raw).size() == 0)
-						return;
+						continue;
 					JSONObject json = (JSONObject) raw;
 					JsonManager.save(json, new File(dataFolder, id + ".json"));
 				}
+				scheduled_saves.clear();
 			}
 		}, 0, 20).getTaskId();
 	}
